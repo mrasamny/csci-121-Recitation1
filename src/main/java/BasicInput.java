92fed5a4d501237fcd2096420a2b3277b24ecf9c
mrasamny <mrasamny@desu.edu>
2024-01-24 23:05:57 -0500

    Initial Commit

--- src/main/java/BasicInput.java
+++ src/main/java/BasicInput.java
@@ -3,12 +3,9 @@ import java.util.Scanner;
 public class BasicInput {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        int userInt = 0;
-        double userDouble = 0.0;
-        // TODO Define char and string variables similarly
 
         System.out.println("Enter integer:");
-        userInt = scnr.nextInt();
+        int userInt = scnr.nextInt();
 
         // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
 
